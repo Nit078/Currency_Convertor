04e45a58dedae04b41d45cd320eda9fd25f1e63e
Nit078 <76880836+Nit078@users.noreply.github.com>
2023-09-04 11:41:13 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,50 @@
+
+
+const populate = async (value, Currency) => {
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_ajir7iOnk6ShlOCMRKeumKtQFFqbfaZKtrmciz3p&base_currency" + Currency
+    let mystr = ""
+    let response = await fetch(url)
+    let rJson = await response.json()
+    console.log(rJson)
+    document.querySelector(."output").style.display="block"
+    for (let key of Object.keys(rJson["data"])) {
+
+        mystr += `
+
+                    <tr>
+                    <td>${key}</td>
+                    <td>${rJson["data"][key]["code"]}</td>
+                    <td>${Math.round(rJson["data"][key]["value"] * value)}</td>
+                    </tr>
+                                `
+    }
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = mystr;
+
+}
+
+
+
+const btn = document.querySelector(".btn")
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    const value = parseInt(document.querySelector("input[name='quantity']").value)
+    const Currency = parseInt(document.querySelector("select[name='Currency']").value)
+    populate(value, Currency)
+})
+
+
+
+
+
+// const tableBody = document.querySelector("tbody");
+// tableBody.innerHTML = `
+
+// <tr>
+// <td>Data1</td>
+// <td>Data2</td>
+// <td>Data3</td>
+// </tr>
+// `
+
